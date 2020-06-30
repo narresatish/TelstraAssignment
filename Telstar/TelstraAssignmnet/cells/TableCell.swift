857e481254a7f2c6c88a544cad9ce1864b388077
Narre Satish Kumar <vishnu.tungena.accionlabs@kony.com>
2020-06-30 10:57:32 +0530

    updated TableCell

--- Telstar/TelstraAssignmnet/cells/TableCell.swift
+++ Telstar/TelstraAssignmnet/cells/TableCell.swift
@@ -10,8 +10,6 @@ import UIKit
 import AlamofireImage
 
 class TableCell: UITableViewCell {
-
-   
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,26 +17,26 @@ class TableCell: UITableViewCell {
     }
     
     var lbltitle: UILabel = {
-              var label = UILabel()
-              label.translatesAutoresizingMaskIntoConstraints = false
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont(name:"HelveticaNeue-Bold", size: 16.0)
-          
-              return label
-          }()
+        
+        return label
+    }()
     var lblDescription: UILabel = {
-           var label = UILabel()
-           label.translatesAutoresizingMaskIntoConstraints = false
-         label.font = UIFont(name:"HelveticaNeue-light", size: 13.0)
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name:"HelveticaNeue-light", size: 13.0)
         label.numberOfLines = 0;
         label.lineBreakMode = .byWordWrapping
-           return label
-       }()
-       
-       var productImage:UIImageView = {
-           var image = UIImageView()
-           image.translatesAutoresizingMaskIntoConstraints = false
-           return image
-       }()
+        return label
+    }()
+    
+    var productImage:UIImageView = {
+        var image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
     
     func setData(data:ModelRows){
         if let title = data.title {
@@ -61,7 +59,7 @@ class TableCell: UITableViewCell {
             productImage.image = nil
         }
     }
-       
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.addSubview(lbltitle)
@@ -69,26 +67,24 @@ class TableCell: UITableViewCell {
         self.addSubview(productImage)
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[v0(110)]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":productImage]))
-         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[v0(110)]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":productImage]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[v0(110)]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":productImage]))
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-125-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":lbltitle]))
-               addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[v0]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":lbltitle]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-5-[v0]", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":lbltitle]))
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-125-[v0]-10-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":lblDescription]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-25-[v0(>=90)]-10-|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0":lblDescription]))
         
-
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
